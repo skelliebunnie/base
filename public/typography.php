75baf2c5d7284325d5083edaa219fdb5ae8d503f
Margaret Ralston <pandahart@gmail.com>
2019-02-26 13:56:22 -0800

    adjusted line-height for heading tags.

--- public/typography.php
+++ public/typography.php
@@ -22,12 +22,20 @@
 			<h6>Header 6, extended length to check line-height</h6>
 		</article>
 		<article class="col">
+			<h1>Header 1</h1>
+			<h2>Header 2</h2>
+			<h3>Header 3</h3>
+			<h4>Header 4</h4>
+			<h5>Header 5</h5>
+			<h6>Header 6</h6>
+		</article>
+		<article class="col">
 			<h1>Header 1, extended length to check line-height</h1>
 			<h2>Header 2, extended length to check line-height</h2>
 			<h3>Header 3, extended length to check line-height</h3>
 			<h4>Header 4, extended length to check line-height</h4>
 			<h5>Header 5, extended length to check line-height</h5>
-			<h6>Header 6, extended length to check line-height</h6>
+			<h6>Header 6, extended length<br>to check line-height</h6>
 		</article>
 	</section>
 </main>
