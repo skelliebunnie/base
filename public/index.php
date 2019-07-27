2e662d5afcac9f08d0697a3ed1bf9c133a5043fb
SkellieBunnie <pandahart@gmail.com>
2019-07-26 17:52:05 -0700

    added hero, changed colors, rearranged some scss.

--- public/index.php
+++ public/index.php
@@ -3,51 +3,33 @@
 	include "templates/header.php";
 	include "templates/sidebar.php";
 ?>
-<main class="site-content pad-xl-around">
-	<nav class="nav">
-		<ul class="nav nav-horizontal">
-			<li class="nav-item bg-alabaster-400 bg-hover-cerulean-700"><a href="#">Test A</a></li>
-			<li class="nav-item bg-alabaster-400 bg-hover-cerulean-700"><a href="#">Test B</a></li>
-			<li class="nav-item bg-alabaster-400 bg-hover-cerulean-700"><a href="#">Test C</a></li>
-			<li class="nav-item bg-alabaster-400 bg-hover-cerulean-700"><a href="#">Test D</a></li>
-		</ul>
-	</nav>
-	<div class="row row2-sm row4-md row6-lg row8-xl text-center">
-		<div class="col bg-cerulean-400">Column 1</div>
-		<div class="col bg-tea-400">Column 2</div>
-		<div class="col bg-carmine-400">Column 3</div>
-		<div class="col bg-neutral-400">Column 4</div>
-		<div class="col bg-alabaster-analogous1-400">Column 5</div>
-		<div class="col bg-alabaster-analogous2-400">Column 6</div>
-		<div class="col bg-alabaster-analogous3-400">Column 7</div>
-		<div class="col bg-alabaster-analogous4-400">Column 8</div>
-	</div>
-	<div class="row">
-		<div class="col col-8 bg-alabaster-100">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero fuga iure voluptate quam blanditiis doloremque, dicta veniam quibusdam. In eius asperiores quasi natus animi rerum illo molestias architecto, fugit eligendi!</p>
-		</div>
-		<div class="col col-4 bg-alabaster-700">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nisi dolorem sit animi molestiae temporibus alias. Eaque et quis iusto optio, cum sit in quidem incidunt. Laboriosam officia nam esse?</p>
+<main class="site-content">
+	<div class="hero-container">
+		<div class="row hero-section hero-header">
+			<div class="col col-12 text-center">
+				<h1>Hero Header</h1>
+			</div>
+			<div class="row">
+				<div class="col col-7 col-margin-lg-y hero-header-content text-lh-lg">
+					<p>Hero Header Content</p>
+					<a href="#" class="btn btn-dark">button<br><span class="text-sm">small</span></a>
+				</div>
+			</div>
 		</div>
 	</div>
-	<!-- <div class="row">
-		<div class="col col-12 text-columns-sm-2 text-columns-lg-4 text-columns-xl-4">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates ut qui earum, ipsam sed fuga recusandae? Culpa dolorem quasi animi sunt, iusto iste ipsam impedit accusantium error, porro dicta laudantium.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed pariatur perferendis repellat iusto quod impedit recusandae nisi esse dolores in, voluptatibus placeat nesciunt, amet est maiores? Veritatis itaque enim autem.</p>
-			<p>Quam explicabo et alias ad quos eligendi dolores, aliquid consequatur nostrum, cumque necessitatibus soluta rerum at ut mollitia sapiente praesentium perferendis veniam laborum nulla maxime. Itaque necessitatibus reprehenderit, fuga quisquam?</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, ab illum rem amet in. Dolor laudantium laborum cum ipsa, fugit asperiores labore ea pariatur quo obcaecati possimus quaerat molestiae tempore!</p>
-			<p>Modi, a. Sequi aperiam voluptatibus accusamus necessitatibus nostrum sed beatae natus consequatur magni autem aliquam distinctio architecto pariatur laudantium ut quos, ipsam. Distinctio temporibus eveniet eum dignissimos at, iusto possimus.</p>
-			<p>Nam dicta perferendis itaque eligendi, hic doloribus fugiat error atque laudantium rem dignissimos magnam incidunt autem quis dolor aliquid, corporis iste aliquam veniam voluptatem odio suscipit architecto vel minus. Quidem.</p>
-			<p>Totam iste quaerat nemo sit dolore! Obcaecati sequi iure dolor, animi, quis inventore doloremque consequatur ducimus vero odio, ut sint quisquam assumenda. Ipsam vero at numquam porro praesentium esse repellendus!</p>
-			<p>Sequi facilis at voluptatibus accusamus est, non hic soluta eaque culpa repellat, delectus eligendi possimus aperiam, rerum aliquam provident nihil repudiandae quidem a fugit et optio asperiores quisquam ut. Corporis!</p>
-			<p>Distinctio quos alias itaque accusantium officiis explicabo incidunt quidem quo animi expedita. Nisi sequi accusantium natus quaerat ab explicabo reiciendis, ratione adipisci rerum beatae impedit quo voluptatem facilis amet minima!</p>
-			<p>Modi blanditiis sequi, fugit consequuntur sed mollitia tempora laudantium! Mollitia recusandae debitis unde maiores perferendis doloremque nesciunt soluta delectus dolores rem eos blanditiis iusto, similique saepe quas vitae, corporis inventore.</p>
-			<p>Quidem molestias voluptatum deleniti explicabo, necessitatibus ratione perferendis reprehenderit pariatur, quisquam et earum facilis, possimus tenetur ut illum! Dolor distinctio dolorum voluptatum amet earum, minus quibusdam at eaque? Fuga, perspiciatis.</p>
-			<p>Eum ex laudantium consequatur totam inventore dolores dolorum error velit illo voluptas cumque exercitationem, voluptatibus maiores dignissimos nihil nisi vel enim aperiam, sequi voluptates! Sapiente, perferendis exercitationem explicabo saepe reprehenderit.</p>
-			<p>Error, incidunt ipsum necessitatibus! Quis similique esse consectetur cumque perferendis quibusdam dolor qui quas, optio reprehenderit impedit necessitatibus, aspernatur iure officia quasi placeat assumenda labore laboriosam omnis. Rerum, et, neque.</p>
-			<p>Sint consectetur voluptate ipsum dignissimos iusto ad laborum aut consequuntur aspernatur omnis saepe, modi libero fugit earum iure autem obcaecati ratione natus quae? Sapiente, aliquam voluptatibus velit mollitia tenetur expedita!</p>
-			<p>Voluptas est nesciunt modi consequatur quo quae reprehenderit consequuntur odit perspiciatis obcaecati repudiandae quidem consectetur delectus, earum iusto provident porro, officia quis. Alias voluptas repellat qui facere dignissimos quas eveniet?</p>
+	<div class="row row2 hero-double">
+		<div class="col col-margin hero-section">
+			<div class='hero-content left md'>
+				<h3>Retaining Wall</h3>
+				<p>Add value and privacy when you replace your worn wood or wire fence with a <em>beautiful</em> and <strong>long-lasting retaining wall</strong>.</p>
+			</div>
 		</div>
-	</div> -->
+		<div class="col col-margin hero-section">
+			<div class='hero-content right md'>
+				<h3>Veneers</h3>
+				<p>Increase the <em>charm and sophistication</em> of your home with tastefully <strong>custom-designed veneers</strong>. <span class="note">Brick & stone available.</span></p>
+			</div>
+		</div>
+	</div>
 </main>
 <?php include "templates/footer.php"; ?>
